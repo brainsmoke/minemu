4328df2544df4ade33fc71292ea19343a5dd05eb
erik <erik@thesishome>
2010-11-09 04:00:06 +0100

    unify indirect jumps

--- emu/jit_code.c
+++ emu/jit_code.c
@@ -451,187 +451,11 @@ int gen_code(char *dst, char *fmt, ...)
 	return j;
 }
 
-int generate_jump(char *dest, char *jmp_addr, trans_t *trans,
-                  char *map, unsigned long map_len)
-{
-	if (contains(map, map_len, jmp_addr))
-	{
-		dest[0] = '\xE9'; /* jmp i32 */
-		*trans = (trans_t){ .jmp_addr=jmp_addr, .imm=1, .len=5 };
-	}
-	else
-	{
-		int stub_len = generate_stub(&dest[6], jmp_addr, &dest[2]);
-
-		dest[0] = '\xFF'; /* jmp *mem */
-		dest[1] = '\x25';
-
-		*trans = (trans_t){ .len=6+stub_len };
-	}
-
-	return trans->len;
-}
-
-static int generate_jcc(char *dest, char *jmp_addr, int cond, trans_t *trans,
-                        char *map, unsigned long map_len)
-{
-	if (contains(map, map_len, jmp_addr))
-	{
-		dest[0] = '\x0F'; /* jcc i32 */
-		dest[1] = '\x80'+cond;
-		*trans = (trans_t){ .jmp_addr=jmp_addr, .imm=2, .len=6 };
-	}
-	else
-	{
-		int stub_len = generate_stub(&dest[8], jmp_addr, &dest[4]);
-
-		dest[0] = '\x70'+ (cond^1); /* j!cc over( jmp *mem ) */
-		dest[1] = '\x06'+stub_len;
-		dest[2] = '\xFF';
-		dest[3] = '\x25';
-
-		*trans = (trans_t){ .len=stub_len+8 };
-	}
-
-	return trans->len;
-}
-
-static int generate_ijump_tail(char *dest, char **jmp_orig, char **jmp_jit)
+static int generate_ijump_tail(char *dest)
 {
 	return gen_code(
 		dest,
 
-		"89 25 L"     /* mov %esp, scratch_stack   */
-		"BC    L"     /* mov $scratch_stack-4 %esp */
-		"9C"          /* pushf                     */
-#ifdef EMU_DEBUG
-"FF 04 25 L"          /* incl ijmp_count           */
-#endif
-		"66 0f 38 17 ed" /* ptest  %xmm5,%xmm5     */
-		"2E 75 12"    /*                           */
-		"3B 05 L"     /* cmp o_addr, %eax          */
-		"2E 75 0B"    /* jne, fall through         */
-		"9D"          /* popf                      */
-		"58"          /* pop %eax                  */
-		"5C"          /* pop %esp                  */
-		"FF 25 L"     /* jmp *j_addr               */
-		"0F 0B"       /* TAINT DETECTED!           */
-#ifdef EMU_DEBUG
-"FF 04 25 L"          /* incl ijmp_misses          */
-#endif
-		"A3    L"     /* mov %eax, o_addr          */
-		"68    L"     /* push j_addr               */
-		"FF 25 L",    /* jmp *runtime_ijmp_addr    */
-
-		scratch_stack,
-		&scratch_stack[-1],
-#ifdef EMU_DEBUG
-&ijmp_count,
-#endif
-		jmp_orig,
-		jmp_jit,
-#ifdef EMU_DEBUG
-&ijmp_misses,
-#endif
-		jmp_orig,
-		jmp_jit,
-		&runtime_ijmp_addr
-	);
-}
-
-static int generate_ijump(char *dest, instr_t *instr, trans_t *trans)
-{
-	char **cache = alloc_jmp_cache(instr->addr);
-	long mrm_len = instr->len - instr->mrm;
-	int i;
-
-	int len_taint = taint_ijmp(dest, &instr->addr[instr->mrm], TAINT_OFFSET);
-	int len = len_taint+gen_code(
-		&dest[len_taint],
-		"A3 L"           /* mov %eax, scratch_stack-4 */
-		"? 8B &$",       /* mov ... ( -> %eax )       */
-		&scratch_stack[-1],
-		instr->p[2], &i, &instr->addr[instr->mrm], mrm_len
-	);
-
-	dest[len_taint+i] &= 0xC7; /* -> %eax */
-	len += generate_ijump_tail(&dest[len], &cache[0], &cache[1]);
-	*trans = (trans_t){ .len = len };
-
-	return len;
-}
-
-static int generate_call_head(char *dest, instr_t *instr, trans_t *trans, int *retaddr_index)
-{
-	int hash = HASH_INDEX(&instr->addr[instr->len]);
-	/* XXX FUGLY as a speed optimisation, we insert the return address
-	 * directly into the cache, this makes relocating code more messy.
-	 * proposed fix: scan memory for call instructions upon relocation,
-	 * change the address in-place
-	 */
-	/* XXX FUGLY translated address is inserted by caller since we don't know
-	 * yet how long the instruction translation will be
-	 */
-	int len_taint = taint_erase_push32(dest, TAINT_OFFSET);
-	int len = len_taint+gen_code(
-		&dest[len_taint],
-
-		"68 L"                /* push $retaddr */
-		"C7 05 L L"           /* movl $addr, jmp_list.addr[HASH_INDEX(addr)] */
-		"C7 05 L & DEADBEEF", /* movl $jit_addr, jmp_list.jit_addr[HASH_INDEX(addr)] */
-
-		&instr->addr[instr->len],
-		&jmp_list.addr[hash],       &instr->addr[instr->len],
-		&jmp_list.jit_addr[hash],   retaddr_index
-	);
-	*retaddr_index += len_taint;
-	return len;
-}
-
-static int generate_icall(char *dest, instr_t *instr, trans_t *trans)
-{
-	int hash = HASH_INDEX(&instr->addr[instr->len]);
-	char **cache = alloc_jmp_cache(instr->addr);
-	long mrm_len = instr->len - instr->mrm;
-	int mrm, retaddr_index;
-
-	/* XXX FUGLY as a speed optimisation, we insert the return address
-	 * directly into the cache, this makes relocating code more messy.
-	 * proposed fix: scan memory for call instructions upon relocation,
-	 * change the address in-place
-	 */
-	int len_taint = taint_icall(dest, &instr->addr[instr->mrm], TAINT_OFFSET);
-	int len = len_taint+gen_code(
-		&dest[len_taint],
-
-		"A3 L"                /* mov %eax, scratch_stack-4 */
-		"? 8B &$"             /* mov ... ( -> %eax )       */
-		"68 L"                /* push $retaddr */
-		"C7 05 L L"           /* movl $addr, jmp_list.addr[HASH_INDEX(addr)] */
-		"C7 05 L & DEADBEEF", /* movl $jit_addr, jmp_list.jit_addr[HASH_INDEX(addr)] */
-
-		&scratch_stack[-1],
-		instr->p[2], &mrm, &instr->addr[instr->mrm], mrm_len,
-		&instr->addr[instr->len],
-		&jmp_list.addr[hash],       &instr->addr[instr->len],
-		&jmp_list.jit_addr[hash],   &retaddr_index
-	);
-
-	dest[len_taint+mrm] &= 0xC7; /* -> %eax */
-	len += generate_ijump_tail(&dest[len], &cache[0], &cache[1]);
-	imm_to(&dest[len_taint+retaddr_index], ((long)dest)+len);
-	*trans = (trans_t){ .len = len };
-	return len;
-}
-
-static int generate_ret(char *dest, char *addr, trans_t *trans)
-{
-	int len = gen_code(
-		dest,
-
-		"66 0F 3A 21 AC 24 L 0E" /* insertps $0xe,TAINT_OFFSET(%esp),%xmm5 */
-		"A3 L"           /* mov %eax, scratch_stack-4 */
-		"58"             /* pop %eax                  */
 		"89 25 L"        /* mov %esp, scratch_stack   */
 		"BC L"           /* mov $scratch_stack-4 %esp */
 		"9C"             /* pushf                     */
@@ -654,8 +478,6 @@ static int generate_ret(char *dest, char *addr, trans_t *trans)
 		"0F 0B"          /* TAINT DETECTED!           */
 		"FF 25 L",       /* jmp *runtime_ijmp_addr    */
 
-		TAINT_OFFSET,
-		&scratch_stack[-1],
 		scratch_stack,
 		&scratch_stack[-1],
 #ifdef EMU_DEBUG
@@ -667,10 +489,6 @@ static int generate_ret(char *dest, char *addr, trans_t *trans)
 		&jit_eip,
 		&runtime_ret_addr
 	);
-
-	*trans = (trans_t){ .len=len };
-
-	return len;
 }
 
 static int generate_ret_cleanup(char *dest, char *addr, trans_t *trans)
@@ -720,6 +538,108 @@ static int generate_ret_cleanup(char *dest, char *addr, trans_t *trans)
 	return len;
 }
 
+static int generate_ijump(char *dest, instr_t *instr, trans_t *trans)
+{
+	long mrm_len = instr->len - instr->mrm;
+	int i;
+
+	int len_taint = taint_ijmp(dest, &instr->addr[instr->mrm], TAINT_OFFSET);
+	int len = len_taint+gen_code(
+		&dest[len_taint],
+		"A3 L"           /* mov %eax, scratch_stack-4 */
+		"? 8B &$",       /* mov ... ( -> %eax )       */
+		&scratch_stack[-1],
+		instr->p[2], &i, &instr->addr[instr->mrm], mrm_len
+	);
+
+	dest[len_taint+i] &= 0xC7; /* -> %eax */
+	len += generate_ijump_tail(&dest[len]);
+	*trans = (trans_t){ .len = len };
+
+	return len;
+}
+
+static int generate_call_head(char *dest, instr_t *instr, trans_t *trans, int *retaddr_index)
+{
+	int hash = HASH_INDEX(&instr->addr[instr->len]);
+	/* XXX FUGLY as a speed optimisation, we insert the return address
+	 * directly into the cache, this makes relocating code more messy.
+	 * proposed fix: scan memory for call instructions upon relocation,
+	 * change the address in-place
+	 */
+	/* XXX FUGLY translated address is inserted by caller since we don't know
+	 * yet how long the instruction translation will be
+	 */
+	int len_taint = taint_erase_push32(dest, TAINT_OFFSET);
+	int len = len_taint+gen_code(
+		&dest[len_taint],
+
+		"68 L"                /* push $retaddr */
+		"C7 05 L L"           /* movl $addr, jmp_list.addr[HASH_INDEX(addr)] */
+		"C7 05 L & DEADBEEF", /* movl $jit_addr, jmp_list.jit_addr[HASH_INDEX(addr)] */
+
+		&instr->addr[instr->len],
+		&jmp_list.addr[hash],       &instr->addr[instr->len],
+		&jmp_list.jit_addr[hash],   retaddr_index
+	);
+	*retaddr_index += len_taint;
+	return len;
+}
+
+static int generate_icall(char *dest, instr_t *instr, trans_t *trans)
+{
+	int hash = HASH_INDEX(&instr->addr[instr->len]);
+	long mrm_len = instr->len - instr->mrm;
+	int mrm, retaddr_index;
+
+	/* XXX FUGLY as a speed optimisation, we insert the return address
+	 * directly into the cache, this makes relocating code more messy.
+	 * proposed fix: scan memory for call instructions upon relocation,
+	 * change the address in-place
+	 */
+	int len_taint = taint_icall(dest, &instr->addr[instr->mrm], TAINT_OFFSET);
+	int len = len_taint+gen_code(
+		&dest[len_taint],
+
+		"A3 L"                /* mov %eax, scratch_stack-4 */
+		"? 8B &$"             /* mov ... ( -> %eax )       */
+		"68 L"                /* push $retaddr */
+		"C7 05 L L"           /* movl $addr, jmp_list.addr[HASH_INDEX(addr)] */
+		"C7 05 L & DEADBEEF", /* movl $jit_addr, jmp_list.jit_addr[HASH_INDEX(addr)] */
+
+		&scratch_stack[-1],
+		instr->p[2], &mrm, &instr->addr[instr->mrm], mrm_len,
+		&instr->addr[instr->len],
+		&jmp_list.addr[hash],       &instr->addr[instr->len],
+		&jmp_list.jit_addr[hash],   &retaddr_index
+	);
+
+	dest[len_taint+mrm] &= 0xC7; /* -> %eax */
+	len += generate_ijump_tail(&dest[len]);
+	imm_to(&dest[len_taint+retaddr_index], ((long)dest)+len);
+	*trans = (trans_t){ .len = len };
+	return len;
+}
+
+static int generate_ret(char *dest, char *addr, trans_t *trans)
+{
+	int len = gen_code(
+		dest,
+
+		"66 0F 3A 21 AC 24 L 0E" /* insertps $0xe,TAINT_OFFSET(%esp),%xmm5 */
+		"A3 L"           /* mov %eax, scratch_stack-4 */
+		"58",            /* pop %eax                  */
+
+
+		TAINT_OFFSET,
+		&scratch_stack[-1]
+	);
+
+	len += generate_ijump_tail(&dest[len]);
+	*trans = (trans_t){ .len=len };
+	return len;
+}
+
 static int generate_linux_sysenter(char *dest, trans_t *trans)
 {
 	int len = gen_code(
@@ -746,29 +666,51 @@ static int generate_int80(char *dest, instr_t *instr, trans_t *trans)
 	return len;
 }
 
-int generate_stub(char *dest, char *jmp_addr, char *imm_addr)
+static int generate_cross_map_jump(char *dest, char *jmp_addr, trans_t *trans)
 {
-	char **cache = alloc_stub_cache(imm_addr, jmp_addr, dest);
-	memcpy(imm_addr, &cache[1], 4);
-	
 	int len = gen_code(
 		dest,
-
-		"89 25 L"        /* mov %esp, scratch_stack   */
-		"BC L"           /* mov $scratch_stack %esp   */
-		"50"             /* push %eax                 */
-		"9C"             /* pushf                     */
-		"B8 L"           /* mov $o_addr, %eax         */
-		"68 L"           /* push j_addr               */
-		"FF 25 L",       /* jmp *runtime_ijmp_addr    */
-
-		scratch_stack,
-		scratch_stack,
-		jmp_addr,
-		&cache[1],
-		&runtime_ijmp_addr);
-
+		"A3 L"           /* mov %eax, scratch_stack-4 */
+		"B8 L",          /* mov jmp_addr, %eax        */
+		&scratch_stack[-1],
+		jmp_addr
+	);
+	len += generate_ijump_tail(&dest[len]);
+	*trans = (trans_t){ .len=len };
 	return len;
+}
+
+int generate_jump(char *dest, char *jmp_addr, trans_t *trans,
+                  char *map, unsigned long map_len)
+{
+	if (contains(map, map_len, jmp_addr))
+	{
+		dest[0] = '\xE9'; /* jmp i32 */
+		*trans = (trans_t){ .jmp_addr=jmp_addr, .imm=1, .len=5 };
+		return trans->len;
+	}
+	else
+		return generate_cross_map_jump(dest, jmp_addr,  trans);
+}
+
+static int generate_jcc(char *dest, char *jmp_addr, int cond, trans_t *trans,
+                        char *map, unsigned long map_len)
+{
+	if (contains(map, map_len, jmp_addr))
+	{
+		dest[0] = '\x0F'; /* jcc i32 */
+		dest[1] = '\x80'+cond;
+		*trans = (trans_t){ .jmp_addr=jmp_addr, .imm=2, .len=6 };
+		return trans->len;
+	}
+	else
+	{
+		int stub_len = generate_cross_map_jump(&dest[2], jmp_addr, trans);
+		trans->len = stub_len+2;
+		dest[0] = '\x70'+ (cond^1); /* j!cc over( jmp *mem ) */
+		dest[1] = '\x06'+stub_len;
+		return trans->len;
+	}
 }
 
 int generate_ill(char *dest, trans_t *trans)
