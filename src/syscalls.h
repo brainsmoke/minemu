7e2b3207397c5051c21c3007888543a3c0ebe599
Erik Bosman <erik@minemu.org>
2011-10-24 08:22:03 +0000

    add set_thread_area syscall

--- src/syscalls.h
+++ src/syscalls.h
@@ -79,6 +79,9 @@ long syscall_intr(long call, long arg1, long arg2, long arg3,
 #define sys_tgkill(a, b, c) \
 	syscall3(SYS_tgkill, (long)(a), (long)(b), (long)(c))
 
+#define sys_set_thread_area(a) \
+	syscall1(SYS_set_thread_area, (long)(a))
+
 #define sys_exit_group(a) \
 	syscall1(SYS_exit_group, (long)(a))
 
