7b141427ca5dd31b17bf0469b256fba7b9b1f783
Erik Bosman <erik@minemu.org>
2011-11-24 22:26:47 +0100

    Put shadow memory mapping operations in separate functions, fix mremap for overlapping regions.

--- src/mm.c
+++ src/mm.c
@@ -35,6 +35,7 @@
 #include "codemap.h"
 #include "load_elf.h"
 #include "kernel_compat.h"
+#include "threads.h"
 
 /* switch when shadow shared memory is completely done */
 //#define SHADOW_DEFAULT_PROT (PROT_NONE)
@@ -42,9 +43,104 @@
 
 unsigned long vdso, vdso_orig, sysenter_reentry, minemu_stack_bottom;
 
+long map_lock;
+
 static int bad_range(unsigned long addr, size_t length)
 {
 	return (addr > USER_END) || (addr+length > USER_END);
+}
+
+static unsigned long min(unsigned long a, unsigned long b) { return a<b ? a:b; }
+static unsigned long max(unsigned long a, unsigned long b) { return a>b ? a:b; }
+
+/* make sure we don't strip implied read permission */
+static long no_exec(long prot)
+{
+	int new_prot = prot & ~PROT_EXEC;
+	if (prot & PROT_EXEC)
+		new_prot |= PROT_READ;
+	return new_prot;
+}
+
+static void shadow_mmap(unsigned long addr, size_t length, long prot, int fd, off_t pgoffset)
+{
+	long ret;
+
+	if (length == 0)
+		return;
+
+	ret = sys_mmap2(addr+TAINT_OFFSET, length, no_exec(prot),
+	                MAP_PRIVATE|MAP_FIXED|MAP_ANONYMOUS, -1, 0);
+
+	if (ret & PG_MASK)
+		die("shadow_m{,un}map(): %08x\n", ret);
+
+	if (prot & PROT_EXEC)
+	{
+		struct kernel_stat64 s;
+		if ( (fd < 0) || (sys_fstat64(fd, &s) != 0) )
+			memset(&s, 0, sizeof(s));
+
+		add_code_region((char *)addr, PAGE_NEXT(length),
+		                s.st_ino, s.st_dev, s.st_mtime, pgoffset);
+	}
+	else
+		del_code_region((char *)addr, PAGE_NEXT(length));
+}
+
+static void shadow_munmap(unsigned long addr, size_t length)
+{
+	shadow_mmap(addr, length, SHADOW_DEFAULT_PROT, -1, 0);
+}
+
+/* mremap() is less orthogonal than one would expect :-( luckily we only need
+ * to handle success.
+ */
+static void shadow_mremap(unsigned long old_addr, size_t old_size,
+                          size_t new_size, long _flags, unsigned long new_addr)
+{
+	long flags = (old_addr != new_addr) ? MREMAP_MAYMOVE|MREMAP_FIXED : 0;
+
+	if (new_addr < old_addr)
+		shadow_munmap(new_addr, min(old_addr-new_addr, new_size));
+
+	if (new_addr+new_size > old_addr+old_size)
+		if (flags)
+			shadow_munmap(max(new_addr, old_addr+old_size),
+			              min(new_size, new_addr+new_size-old_addr-old_size));
+		else if (new_size != old_size)
+		{
+			mutex_lock(&map_lock); /* make sure user_mmap2() will not return memory
+			                        * from our shadow memory hole.
+			                        */
+			sys_munmap(old_addr+old_size+TAINT_OFFSET, new_size-old_size);
+		}
+
+	if ( (new_addr != old_addr) || (new_size != old_size) )
+	{
+		long ret = sys_mremap(old_addr+TAINT_OFFSET, old_size, new_size, flags,
+		                      new_addr+TAINT_OFFSET);
+
+		if (!flags)
+			mutex_unlock(&map_lock); /* release lock before we may do other locking
+			                          * to prevent deadlocks.
+			                          */
+
+		if (ret & PG_MASK)
+			die("shadow_mremap(): %08x\n", ret);
+
+		if (find_code_map((char *)old_addr))
+			del_code_region((char *)new_addr, PAGE_NEXT(new_size));
+		else
+			add_code_region((char *)new_addr, PAGE_NEXT(new_size), 0, 0, 0, 0);
+	}
+
+	if (old_addr < new_addr)
+		shadow_munmap(old_addr, min(new_addr-old_addr, old_size));
+
+	if (old_addr+old_size > new_addr+new_size)
+		shadow_munmap(max(old_addr, new_addr+new_size),
+		              min(old_size, old_addr+old_size-new_addr-new_size));
 }
 
 unsigned long do_mmap2(unsigned long addr, size_t length, int prot,
@@ -103,36 +199,18 @@ unsigned long user_mmap2(unsigned long addr, size_t length, int prot,
 	if ( bad_range(addr, length) )
 		return -EFAULT;
 
-	int new_prot = prot; /* make sure we don't strip implied read permission */
-	if (prot & PROT_EXEC)
-		new_prot = (prot & ~PROT_EXEC) | PROT_READ;
-
-	unsigned long ret = sys_mmap2(addr, length, new_prot, flags, fd, pgoffset);
+	/* shadow_mremap() might temporarily leave holes in shadow memory
+	 * make sure we won't get this memory.
+	 */
+	mutex_lock(&map_lock);
+	unsigned long ret = sys_mmap2(addr, length, no_exec(prot),
+	                              flags, fd, pgoffset);
+	mutex_unlock(&map_lock);
 
 	if ( !(ret & PG_MASK) )
-	{
-		sys_mmap2(ret+TAINT_OFFSET, length, new_prot, MAP_PRIVATE|MAP_FIXED|MAP_ANONYMOUS, -1, 0);
-		if (prot & PROT_EXEC)
-		{
-			struct kernel_stat64 s;
-			if ( (fd < 0) || (sys_fstat64(fd, &s) != 0) )
-				memset(&s, 0, sizeof(s));
-
-			add_code_region((char *)ret, PAGE_NEXT(length),
-			                s.st_ino, s.st_dev, s.st_mtime, pgoffset);
-		}
-		else
-			del_code_region((char *)ret, PAGE_NEXT(length));
-	}
+		shadow_mmap(ret, length, prot, fd, pgoffset);
 
 	return ret;
-}
-
-unsigned long shadow_munmap(unsigned long addr, size_t length)
-{
-	del_code_region((char *)addr, PAGE_NEXT(length));
-	return sys_mmap2(addr+TAINT_OFFSET, length, SHADOW_DEFAULT_PROT,
-	                 MAP_PRIVATE|MAP_FIXED|MAP_ANONYMOUS, -1, 0);
 }
 
 unsigned long user_munmap(unsigned long addr, size_t length)
@@ -153,8 +231,8 @@ unsigned long user_mprotect(unsigned long addr, size_t length, long prot)
 	if ( bad_range(addr, length) )
 		return -EFAULT;
 
-	unsigned long ret = sys_mprotect(addr, length, prot&~PROT_EXEC);
-	                    sys_mprotect(TAINT_OFFSET+addr, length, prot&~PROT_EXEC);
+	unsigned long ret = sys_mprotect(addr, length, no_exec(prot));
+	                    sys_mprotect(TAINT_OFFSET+addr, length, no_exec(prot));
 
 	if ( !(ret & PG_MASK) )
 	{
@@ -189,36 +267,11 @@ unsigned long user_mremap(unsigned long old_addr, size_t old_size,
 	if ( (flags & MREMAP_FIXED) && bad_range(new_addr, new_size) )
 		return -ENOMEM;
 	
-	int is_code = !!find_code_map((char *)old_addr);
 	unsigned long ret = sys_mremap(old_addr, old_size, new_size, flags, new_addr);
 
 	if (! (ret & PG_MASK) )
-	{
-		sys_mremap(old_addr+TAINT_OFFSET, old_size, new_size,
-		           MREMAP_MAYMOVE|MREMAP_FIXED, ret+TAINT_OFFSET);
+		shadow_mremap(old_addr, old_size, new_size, flags, ret);
 
-		if (old_addr < ret)
-		{
-			unsigned long size = ret-old_addr;
-			if (size > old_size)
-				size = old_size;
-			shadow_munmap(old_addr, size);
-		}
-
-		if (old_addr+old_size > ret+new_size)
-		{
-			unsigned long size = (old_addr+old_size)-(ret+new_size);
-			if (size > old_size)
-				size = old_size;
-			shadow_munmap(old_addr+old_size - size, size);
-		}
-
-		if (is_code)
-			del_code_region((char *)ret, PAGE_NEXT(new_size));
-		else
-			add_code_region((char *)ret, PAGE_NEXT(new_size), 0, 0, 0, 0);
-
-	}
 	return ret;
 }
 
@@ -267,6 +320,7 @@ void init_minemu_mem(long *auxv)
 	char c[1];
 
 	fill_last_page_hack();
+	mutex_init(&map_lock);
 
 	/* pre-allocate some memory regions, mostly because this way we don't
 	 * have to do our own memory-allocation. It /is/ the reason we need
